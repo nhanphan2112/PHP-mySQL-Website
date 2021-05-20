2b4c0e070c683d8f8ef0d1d301c128a6fdf0ccf8
Nhan Phan <nphanacc@gmail.com>
2021-05-20 10:14:32 -0700

    08-addslash in php to check for aspostrophes

--- root/add_joke.php
+++ root/add_joke.php
@@ -4,6 +4,9 @@ include "db_connect.php";
 $new_joke_question = $_GET["newjoke"];
 $new_joke_answer = $_GET["newanswer"];
 
+$new_joke_question = addslashes($new_joke_question);
+$new_joke_answer = addslashes($new_joke_answer);
+
 // Search the database for the keyword 
 echo "<h2>Trying to add a new joke: $new_joke_question and $new_joke_answer</h2>";
 
@@ -12,7 +15,7 @@ echo "<h2>Trying to add a new joke: $new_joke_question and $new_joke_answer</h2>
 $sql = "INSERT INTO Jokes_table (JokeID, Joke_question, Joke_answer) VALUES (NULL, '$new_joke_question', '$new_joke_answer' )" ;
 
 //echo "$sql";
-$result = $mysqli->query($sql);
+$result = $mysqli->query($sql) or die(mysqli_error($mysqli));
 include "search_all_jokes.php";
 
 ?>
